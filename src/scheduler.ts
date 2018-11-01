8ca6a9b0eb893e824815eaa52df870c13089fddf
Tao Wen <taowen@gmail.com>
2018-11-01 21:27:06 +0800

    call and serve can happen in any order

--- src/scheduler.ts
+++ src/scheduler.ts
@@ -1,39 +1,100 @@
 import {v4 as uuid} from 'uuid'
 
-type Server = {
-    methods: { [key: string]: Function }
+interface Task {
+    taskId: string
     resolve: Resolve
     reject: Reject
 }
+
 type Resolve = (obj: any) => void
 type Reject = (obj: any) => void
 
+interface Server extends Task {
+    methods: { [key: string]: Function }
+}
+
+interface ServeResult {
+    methodName: string
+    methodArgs: any[]
+    returnValue: any
+}
+
+interface Client extends Task {
+    methodName: string
+    methodArgs: any[]
+}
+
 let scheduler = new (class {
 
-    servers: { [key: string]: Server }
+    servers: { [serverId: string]: Server }
+    clients: { [serverId: string]: Client[] }
 
     constructor() {
         this.servers = {}
+        this.clients = {}
     }
 
-    serve(actor: Actor, methods: { [key: string]: Function }) {
+    serve(serverId: string, methods: { [key: string]: Function }): Promise<ServeResult> {
+        let clients = this.clients[serverId]
+        while (clients && clients.length > 0) {
+            let client = clients.pop()
+            let method = methods[client.methodName]
+            if (!method) {
+                client.reject('method unknown: ' + client.methodName)
+                continue
+            }
+            let returnValue = method.apply(this, client.methodArgs)
+            client.resolve(returnValue)
+            return Promise.resolve({
+                methodName: client.methodName,
+                methodArgs: client.methodArgs,
+                returnValue: returnValue
+            })
+        }
         return new Promise((resolve, reject) => {
-            this.servers[actor.id] = {
-                methods: methods,
+            this.servers[serverId] = {
+                taskId: serverId,
                 resolve: resolve,
-                reject: reject
+                reject: reject,
+                methods: methods,
             }
         })
     }
 
-    call(caller: Actor, callee: Actor, method_name: string, ...args: any[]) {
-        let server = this.servers[callee.id]
-        let method = server.methods[method_name]
-        return method.apply(this, args)
+    call(callerId: string, calleeId: string, methodName: string, ...methodArgs: any[]) {
+        let server = this.servers[calleeId]
+        if (!server) {
+            return new Promise((resolve, reject) => {
+                let clients = this.clients[calleeId] = this.clients[calleeId] || []
+                clients.push({
+                    taskId: callerId,
+                    resolve: resolve,
+                    reject: reject,
+                    methodName: methodName,
+                    methodArgs: methodArgs,
+                })
+            })
+        }
+        let method = server.methods[methodName]
+        if (!method) {
+            return Promise.reject('method unknown: ' + methodName)
+        }
+        let returnValue = method.apply(this, methodArgs)
+        server.resolve({
+            methodName: methodName,
+            methodArgs: methodArgs,
+            returnValue: returnValue
+        })
+        return Promise.resolve(returnValue)
     }
 
     sleep(actor: Actor, duration: number): Promise<void> {
         return new Promise(resolve => setTimeout(resolve, duration))
+    }
+
+    reset() {
+        this.servers = {}
+        this.clients = {}
     }
 })()
 
@@ -47,6 +108,6 @@ export class Actor {
 
     constructor(f: Function, ...args: any[]) {
         this.id = uuid()
-        this.result = f.apply(this, args)
+        this.result = f.apply(this.id, args)
     }
 }
--- test/call.test.ts
+++ test/call.test.ts
@@ -1,5 +1,9 @@
 import scheduler, {Actor} from "../src/scheduler"
 
+beforeEach(() => {
+    scheduler.reset();
+});
+
 describe("scheduler call", () => {
     it("serve before call", async () => {
         let server = new Actor(async function () {
@@ -10,22 +14,55 @@ describe("scheduler call", () => {
         })
         let client = new Actor(async function () {
             let me = this
-            return await scheduler.call(me, server, 'add_one', 1)
+            return await scheduler.call(me, server.id, 'add_one', 1)
         })
         expect(await client.result).toEqual(2)
     });
     it("call before serve", async () => {
-        let server = new Actor(async function() {
+        let server = new Actor(async function () {
             let me = this
-            await scheduler.sleep(me, 1000)
+            await scheduler.sleep(me, 100)
             await scheduler.serve(me, {
                 add_one: (val: number) => val + 1
             })
         })
-        let client = new Actor(async function(server: Actor) {
+        let client = new Actor(async function () {
             let me = this
-            return await scheduler.call(me, server, 'add_one', 1)
+            return await scheduler.call(me, server.id, 'add_one', 1)
         })
         expect(await client.result).toEqual(2)
+    })
+    it("call not existing method after serve", async () => {
+        let server = new Actor(async function () {
+            let me = this
+            await scheduler.serve(me, {})
+        })
+        let client = new Actor(async function () {
+            let me = this
+            return await scheduler.call(me, server.id, 'add_one', 1)
+        })
+        try {
+            await client.result
+            fail()
+        } catch (e) {
+            expect(e).not.toBeNull()
+        }
+    })
+    it("call not existing method before serve", async () => {
+        let server = new Actor(async function () {
+            let me = this
+            await scheduler.sleep(me, 100)
+            await scheduler.serve(me, {})
+        })
+        let client = new Actor(async function () {
+            let me = this
+            return await scheduler.call(me, server.id, 'add_one', 1)
+        })
+        try {
+            await client.result
+            fail()
+        } catch (e) {
+            expect(e).not.toBeNull()
+        }
     })
 });
