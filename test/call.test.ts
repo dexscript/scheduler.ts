78bdf788fb52b39db734b3cca818d93eff7a8146
Tao Wen <taowen@gmail.com>
2018-11-01 20:13:25 +0800

    add test of call before serve

--- test/call.test.ts
+++ test/call.test.ts
@@ -2,20 +2,30 @@ import scheduler, {Actor} from "../src/scheduler"
 
 describe("scheduler call", () => {
     it("serve before call", async () => {
-        let b = new Actor(B)
-        let a = new Actor(A, b)
-        expect(await a.result).toEqual(2)
+        let server = new Actor(async function () {
+            let me = this
+            await scheduler.serve(me, {
+                add_one: (val: number) => val + 1
+            })
+        })
+        let client = new Actor(async function () {
+            let me = this
+            return await scheduler.call(me, server, 'add_one', 1)
+        })
+        expect(await client.result).toEqual(2)
     });
-});
-
-async function A(b: Actor) {
-    let me = this
-    return await scheduler.call(me, b, 'add_one', 1)
-}
-
-async function B() {
-    let me = this
-    await scheduler.serve(me, {
-        add_one: (val: number) => val + 1
+    it("call before serve", async () => {
+        let server = new Actor(async function() {
+            let me = this
+            await scheduler.sleep(me, 1000)
+            await scheduler.serve(me, {
+                add_one: (val: number) => val + 1
+            })
+        })
+        let client = new Actor(async function(server: Actor) {
+            let me = this
+            return await scheduler.call(me, server, 'add_one', 1)
+        })
+        expect(await client.result).toEqual(2)
     })
-}
+});
