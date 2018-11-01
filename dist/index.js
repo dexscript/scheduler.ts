4a22e180810c30f0f62bb974f29a95b5826bc3df
Tao Wen <taowen@gmail.com>
2018-11-01 19:37:58 +0800

    make scheduler.call work

--- dist/index.js
+++ dist/index.js
@@ -1,23 +1,38 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const uuid_1 = require("uuid");
-console.log(uuid_1.v4());
 let scheduler = new (class {
     constructor() {
+        this.servers = {};
     }
     serve(actor, methods) {
-        return new Promise(resolve => resolve());
+        return new Promise((resolve, reject) => {
+            this.servers[actor.id] = {
+                methods: methods,
+                resolve: resolve,
+                reject: reject
+            };
+        });
+    }
+    call(caller, callee, method_name, ...args) {
+        let server = this.servers[callee.id];
+        let method = server.methods[method_name];
+        return method.apply(this, args);
     }
     sleep(actor, duration) {
         return new Promise(resolve => setTimeout(resolve, duration));
     }
 })();
 class Actor {
-    constructor(f) {
+    constructor(f, ...args) {
+        this.id = uuid_1.v4();
+        f.apply(this, args);
     }
 }
 async function A(b) {
     let me = this;
+    let result = await scheduler.call(me, b, 'add_one', 1);
+    console.log(result);
 }
 async function B() {
     let me = this;
@@ -29,5 +44,6 @@ async function B() {
         sub_one: (val) => val - 1
     });
 }
-B();
+let b = new Actor(B);
+new Actor(A, b);
 //# sourceMappingURL=index.js.map
